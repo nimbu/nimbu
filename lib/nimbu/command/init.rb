597b6e2014e3ab6c9e522338c73dc385510e3f47
Peter Dedene <peter@zenjoy.be>
2012-02-23 13:20:08 +0100

    Added directories to init command

--- lib/nimbu/command/init.rb
+++ lib/nimbu/command/init.rb
@@ -9,10 +9,28 @@ class Nimbu::Command::Init < Nimbu::Command::Base
   # log in with your nimbu credentials
   #
   def index
-    display "Initialize the Nimbu configuration file."
-    config = Nimbu::Auth.get_configuration
-    display "Configuration ready: #{config}"
-    config = Nimbu::Auth.get_credentials
+    if Nimbu::Auth.read_configuration
+      print green(bold("CONGRATULATIONS!")), ": this directory is already configured as a Nimbu theme."
+    else
+      display "Initialize the Nimbu configuration file."
+      config = Nimbu::Auth.get_configuration
+
+      display "Configuration ready: #{config}"
+      config = Nimbu::Auth.get_credentials
+
+      display "Initializing directories:"
+      display " - layouts"
+      FileUtils.mkdir_p(File.join(Dir.pwd,'layouts'))
+      display " - templates"
+      FileUtils.mkdir_p(File.join(Dir.pwd,'templates'))
+      display " - stylesheets"
+      FileUtils.mkdir_p(File.join(Dir.pwd,'stylesheets'))
+      display " - javascripts"
+      FileUtils.mkdir_p(File.join(Dir.pwd,'javascripts'))
+      display " - images"
+      FileUtils.mkdir_p(File.join(Dir.pwd,'images'))
+      print green(bold("Done. Happy coding!\n"))
+    end
   end
 end
 
