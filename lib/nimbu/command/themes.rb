f7022f4876cf39ea107f5c5091af0d7d466455ba
Peter Dedene <peter@zenjoy.be>
2012-03-04 15:35:32 +0100

    Selective uploading of assets

--- lib/nimbu/command/themes.rb
+++ lib/nimbu/command/themes.rb
@@ -90,66 +90,77 @@ class Nimbu::Command::Themes < Nimbu::Command::Base
   #
   def push
     simulate = args.include?("--dry-run") || args.include?("-d")
-    input = args.shift.downcase rescue nil
-    if !input.to_s.strip.empty?
-      theme = input.to_s.strip
-    else
-      theme = Nimbu::Auth.theme
-    end
+    css_only = args.include?("--css-only") || args.include?("--css")
+    js_only = args.include?("--js-only") || args.include?("-js")
+
+    # if !input.to_s.strip.empty?
+    #   theme = input.to_s.strip
+    # else
+    # end
+    theme = Nimbu::Auth.theme
     display "Pushing layouts, templates and assets for '#{theme}' to the server:"
     
     layouts_glob = Dir.glob("#{Dir.pwd}/layouts/**/*.liquid")
     layouts_files = layouts_glob.map {|dir| dir.gsub("#{Dir.pwd}/layouts/","")}
-    print "\nLayouts:\n"
-    layouts_files.each do |layout|
-      file = "#{Dir.pwd}/layouts/#{layout}"
-      next if File.directory?(file)
-      print " - layouts/#{layout}"
-      nimbu.upload_layout(theme, layout, IO.read(file))
-      print " (ok)\n"
-    end      
-
     templates_glob = Dir.glob("#{Dir.pwd}/templates/**/*.liquid")
     templates_files = templates_glob.map {|dir| dir.gsub("#{Dir.pwd}/templates/","")}
-    print "\nTemplates:\n"
-    templates_files.each do |template|
-      file = "#{Dir.pwd}/templates/#{template}"
-      next if File.directory?(file)
-      print " - templates/#{template}"
-      nimbu.upload_template(theme, template, IO.read(file))
-      print " (ok)\n"
+
+    if !(css_only || js_only)
+      print "\nLayouts:\n"
+      layouts_files.each do |layout|
+        file = "#{Dir.pwd}/layouts/#{layout}"
+        next if File.directory?(file)
+        print " - layouts/#{layout}"
+        nimbu.upload_layout(theme, layout, IO.read(file))
+        print " (ok)\n"
+      end      
+
+      print "\nTemplates:\n"
+      templates_files.each do |template|
+        file = "#{Dir.pwd}/templates/#{template}"
+        next if File.directory?(file)
+        print " - templates/#{template}"
+        nimbu.upload_template(theme, template, IO.read(file))
+        print " (ok)\n"
+      end
     end
 
     css_glob = Dir.glob("#{Dir.pwd}/stylesheets/**/*.css")
     css_files = css_glob.map {|dir| dir.gsub("#{Dir.pwd}/stylesheets/","")}
-    print "\nStylesheet:\n"
-    css_files.each do |css|
-      file = "#{Dir.pwd}/stylesheets/#{css}"
-      next if File.directory?(file) || (!anyFileWithWord?(layouts_glob,css) && !anyFileWithWord?(templates_glob,css))
-      nimbu.upload_asset(theme, "stylesheets/#{css}", File.open(file))
-      print " - stylesheets/#{css}"
-      print " (ok)\n"
+    if !js_only
+      print "\nStylesheet:\n"
+      css_files.each do |css|
+        file = "#{Dir.pwd}/stylesheets/#{css}"
+        next if File.directory?(file) || (!anyFileWithWord?(layouts_glob,css) && !anyFileWithWord?(templates_glob,css))
+        nimbu.upload_asset(theme, "stylesheets/#{css}", File.open(file))
+        print " - stylesheets/#{css}"
+        print " (ok)\n"
+      end
     end
 
     js_glob = Dir.glob("#{Dir.pwd}/javascripts/**/*.js")
     js_files = js_glob.map {|dir| dir.gsub("#{Dir.pwd}/javascripts/","")}
-    print "\nJavascripts:\n"
-    js_files.each do |js|
-      file = "#{Dir.pwd}/javascripts/#{js}"
-      next if File.directory?(file) || (!anyFileWithWord?(layouts_glob,js) && !anyFileWithWord?(templates_glob,js))
-      nimbu.upload_asset(theme, "javascripts/#{js}", File.open(file))
-      print " - javascripts/#{js}"
-      print " (ok)\n"
+    if !css_only
+      print "\nJavascripts:\n"
+      js_files.each do |js|
+        file = "#{Dir.pwd}/javascripts/#{js}"
+        next if File.directory?(file) || (!anyFileWithWord?(layouts_glob,js) && !anyFileWithWord?(templates_glob,js))
+        nimbu.upload_asset(theme, "javascripts/#{js}", File.open(file))
+        print " - javascripts/#{js}"
+        print " (ok)\n"
+      end
     end
 
     image_files = Dir.glob("#{Dir.pwd}/images/**/*").map {|dir| dir.gsub("#{Dir.pwd}/images/","")}
-    print "\nImages:\n"
-    image_files.each do |image|
-      file = "#{Dir.pwd}/images/#{image}"
-      next if File.directory?(file) || (!anyFileWithWord?(css_glob,image) && !anyFileWithWord?(js_glob,image) && !anyFileWithWord?(layouts_glob,image) && !anyFileWithWord?(templates_glob,image))
-      nimbu.upload_asset(theme, "images/#{image}", File.open(file))
-      print " - images/#{image}"
-      print " (ok)\n"
+    if !(css_only || js_only)
+      print "\nImages:\n"
+      image_files.each do |image|
+        file = "#{Dir.pwd}/images/#{image}"
+        next if File.directory?(file) || (!anyFileWithWord?(css_glob,image) && !anyFileWithWord?(js_glob,image) && !anyFileWithWord?(layouts_glob,image) && !anyFileWithWord?(templates_glob,image))
+        nimbu.upload_asset(theme, "images/#{image}", File.open(file))
+        print " - images/#{image}"
+        print " (ok)\n"
+      end
     end
   end
 
